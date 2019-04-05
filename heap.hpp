24dc289c3eab8e180b9d15bfc0b073ac459ee37a
Ivan <wmj900823@gmail.com>
2019-04-05 10:33:06 -0700

    implemented heapify, pop, push, is_emptry, clear and <<

---
+++ heap.hpp
@@ -0,0 +1,85 @@
+//
+// Created by Ivan on 2019-04-02.
+//
+
+#ifndef HEAP_HEAP_HPP
+#define HEAP_HEAP_HPP
+
+#include <vector>
+#include <ostream>
+#include <algorithm>
+
+template<class T>
+class heap {
+
+private:
+    std::vector<T> values;
+
+    /**
+     * heapify the heap from the index specified
+     * @param index starting index
+     */
+    void heapify(){
+        std::make_heap(values.begin(), values.end());
+    };
+
+public:
+    heap() = default;
+    heap(typename std::vector<T>::iterator first, typename std::vector<T>::iterator last){
+        for (auto it = first; it != last; it++) {
+            values.push_back(*it);
+        }
+        heapify();
+    };
+
+    /**
+     * add new element to heap
+     * @param value
+     */
+    void push(T value) {
+        values.push_back(value);
+        //maintain the heap
+        std::push_heap(values.begin(), values.end());
+    };
+
+    /**
+     * remove  and return the root
+     * @return root value(max value)
+     */
+    T pop() {
+        //move first to the last - 1, and maintain the heap
+        std::pop_heap(values.begin(), values.end());
+        //get the value of previous root
+        T value = values.back();
+        //remove the previous root
+        values.pop_back();
+        //return the previous root
+        return value;
+    };
+
+    /**
+     *
+     * @return the size of the heap
+     */
+    size_t size() const { return values.size(); };
+
+    /**
+     *
+     * @return true, if the heap is empty, otherwise, false
+     */
+    bool is_empty() const { return values.empty(); };
+
+    /**
+     * clear the heap, and make the size of the vector to be 0
+     */
+    void clear() { std::vector<T>().swap(values); };
+
+    friend std::ostream &operator<<(std::ostream &os, const heap &h) {
+        std::for_each(h.values.begin(), h.values.end(), [&os](const T &t) {
+            os << t << "  ";
+        });
+        return os;
+    }
+};
+
+#endif //HEAP_HEAP_HPP
--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "heap.hpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    heap<int> h;
+
+    h.push(1);
+    h.push(2);
+    h.push(3);
+    h.push(4);
+    h.push(5);
+    h.push(6);
+
+    std::cout << h << std::endl;
+    std::cout << h.pop() << std::endl;
+
+    std::cout << h << std::endl;
+    std::cout << h.size() << ", ";
+    std::cout << h.pop() << std::endl;
+
+    std::cout << h << std::endl;
+
+    std::cout << std::boolalpha << h.is_empty() << "\n";
+    h.clear();
+    std::cout << "print..." << h << "\n";
+    std::cout << std::boolalpha << h.is_empty() << "\n";
+
+
+
     return 0;
 }
